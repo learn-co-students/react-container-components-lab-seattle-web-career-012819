14efcb6203d90b1a46f9815d2887dea1201d5484
ScottDenton <scottyd_14@hotmail.com>
2019-04-06 08:44:32 -0700

    working but commented out movie review tests

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = '';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'N2erDi30ylvZUw4CHn0n83YHVTL0uEUu';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'+ `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      reviews:[]
+    }
+  }
+
+
+componentDidMount(){
+  fetch(URL)
+  .then(res => res.json())
+  .then(reviews => {
+    this.setState({
+      reviews: reviews.results.map((movie) => {
+      return   movie
+      })
+    })
+  })
+}
+
+
+  render () {
+    const reviewList = this.state.reviews.map(thisMovie => {
+      return <MovieReviews movie={thisMovie} key={thisMovie.headline}/>
+    });
+
+    return(
+      <div className='latest-movie-reviews'>
+        <div className="review-list">
+          {reviewList}
+        </div>
+      </div>
+
+    )
+
+  }
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = '';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'N2erDi30ylvZUw4CHn0n83YHVTL0uEUu';
+const URL = 'https://api.nytimes.com/svc/reviews/v2/reviews/search.json?';
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      reviews:[],
+      searchTerm: ''
+    }
+  }
+
+fetchMovies = () =>{
+  fetch(URL + `query=${this.state.searchTerm}&` + `api-key=${NYT_API_KEY}`)
+  .then(res => res.json())
+  .then(reviews => {
+    this.setState({
+      reviews: reviews.results.map((movie) => {
+      return  movie
+      })
+    })
+  })
+}
+
+handleSubmission =(e) => {
+  e.preventDefault();
+  this.setState({
+    searchTerm: document.getElementById("search-term").value
+  })
+  this.fetchMovies()
+}
+
+
+render(){
+  const reviewList = this.state.reviews.map(thisMovie => {
+    return <MovieReviews movie={thisMovie} key={thisMovie.headline}/>
+  });
+
+  return(
+    <div className ="searchable-movie-reviews">
+      <form onSubmit={this.handleSubmission}>
+        <input type="text" id="search-term" placeholder = 'search for a movie'/>
+        <button type ='Submit' > Search </button>
+      </form>
+          <div className='searchable-movie-reviews'>
+            <div className="review-list">
+              {reviewList}
+            </div>
+          </div>
+    </div>
+  )
+}
+}
