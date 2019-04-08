e24feec3e697824f7637287a313ba8b65cbb8646
github username <crishanks12@gmail.com>
2019-04-08 11:13:07 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+
+  return (
+    <ul>
+      <h4> Title: {props.review.display_title} </h4>
+      <li> Rating: {props.review.mpaa_rating} </li>
+      <li> Author: {props.review.byline} </li>
+      <li> Headline: {props.review.headline} </li>
+      <li> Summary: {props.review.summary_short} </li>
+    </ul>
+  )
+}
+
+export default MovieReviews
